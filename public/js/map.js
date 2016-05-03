7d14aefd351e05adef38945eb53834b6934b5a08
shree33 <skbhanderi3@gmail.com>
2016-05-03 18:08:51 -0400

    bug fix

--- public/js/map.js
+++ public/js/map.js
@@ -110,8 +110,8 @@ function sendData(){
 
           //console.log('yo');
           var url = "http://crumbtrail.herokuapp.com/search";
-          //console.log(fbID);
-          //console.log(fbID.ID);
+          console.log(fbID);
+          console.log(fbID.ID);
           var params = "userID=" + fbID + "&foodtype=" + inputs.preference + "&startpoint="+ inputs.locationFrom 
                                                                                 + "&endpoint=" + inputs.locationTo;
           http.open("POST", url, true);
