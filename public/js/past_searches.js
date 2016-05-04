46241a0fbd36927bd4891dd1618c445e17c0f41b
Alyssa Fusillo <alyssa.fusillo@tufts.edu>
2016-05-04 09:56:59 -0400

    got rid of console messages

--- public/js/past_searches.js
+++ public/js/past_searches.js
@@ -13,9 +13,6 @@
 
 	       for (var i = 0; i < arr.length; i++) {
 	       		var locationTo = JSON.parse(arr[i].startpoint)[0].formatted_address;
-	       		console.log((arr[i].startpoint)[0].formatted_address);
-	       		console.log(locationTo);
-
 	       		var locationFrom = JSON.parse(arr[i].endpoint)[0].formatted_address;
 	       		console.log(locationFrom);
 	       		var preference = arr[i].foodtype;
